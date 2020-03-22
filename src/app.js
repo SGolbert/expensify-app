bbf16a6b7dfab72c0911ad562fd54f1fb6f16752
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-03-22 20:28:07 +0100

    finish up app

--- src/app.js
+++ src/app.js
@@ -5,14 +5,7 @@ import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./stores/configureStore";
 import { startSetExpenses } from "./actions/expenses";
 import { login, logout } from "./actions/auth";
-// import {
-//   setEndDate,
-//   setFilterText,
-//   setStartDate,
-//   sortByAmount,
-//   sortByDate
-// } from "./actions/filters";
-// import getVisibleExpenses from "./selectors/expenses";
+import LoadingPage from "./components/LoadingPage";
 import "normalize.css/normalize.css";
 import "./styles/style.scss";
 import "react-dates/lib/css/_datepicker.css";
@@ -34,7 +27,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import getTotalExpenses from "../selectors/expenses-total";
 import selectExpenses from "../selectors/expenses";
 import numeral from "numeral";
 
 export const ExpensesSummary = props => {
-  if (props.expenses.length === 0) {
-    return <p></p>;
-  } else {
-    return (
-      <div>
-        <p>
-          Viewing {props.expenses.length} expense
-          {props.expenses.length > 1 ? "s" : ""} totalling{" "}
-          {numeral(getTotalExpenses(props.expenses) / 100).format("$0,0.00")}
-        </p>
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{props.expenses.length}</span> expense
+          {props.expenses.length != 1 ? "s" : ""} totalling{" "}
+          <span>
+            {numeral(getTotalExpenses(props.expenses) / 100).format("$0,0.00")}
+          </span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">
+            Add Expense
+          </Link>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
